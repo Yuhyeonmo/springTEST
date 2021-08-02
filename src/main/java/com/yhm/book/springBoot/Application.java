6b9ce474cc529d6d9de2561cb9db14a319ac6959
YuhyeonMo <kora1492@naver.com>
2021-08-02 23:36:42 +0900

    YHM03 - 20210802 restart commit HelloController, HelloControllerTest ADD + bulid.gradle 수정 401 Error(secure = false)

--- src/main/java/com/yhm/book/springBoot/Application.java
+++ src/main/java/com/yhm/book/springBoot/Application.java
@@ -2,7 +2,8 @@ package com.yhm.book.springBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Application 클래스는 Main Class
+// @SpringBootApplication의 스프링 부트의 자동설정, 스프링 bean 읽기와 생성 모두 자동으로 설정 / 이클래스는 항상 프로젝트 최상단에 위치함.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
